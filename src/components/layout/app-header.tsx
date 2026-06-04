266b5e35c899e5f5deab4020992f98a6a23c765f
Pedro Ecsedei <pedro.martins55757@gmail.com>
2026-06-04 18:36:06 -0300

    chore: adjusting logo

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -3,12 +3,10 @@ import { Logo } from "../logo";
 
 export function AppHeader() {
   return (
-    <header className="border-b border-gray-300 flex items-center">
-      <h1 className="font-semibold">
-        <Link className="text-xl font-bold" href="/">
-          <Logo />
-        </Link>
-      </h1>
+    <header className="border-b border-gray-300 flex items-center justify-between px-6 py-4 ">
+      <Link className="text-xl font-bold" href="/">
+        <Logo />
+      </Link>
     </header>
   );
 }
